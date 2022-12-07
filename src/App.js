c6591830812a29875cdb93b97dc938fa54c3e199
Aakanksha <shindeaakanksha28@gmail.com>
2022-12-07 16:14:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import glogo from './github_logo.png'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>
+        <img src="https://i.imgur.com/Kagbzkq.png"/>
+        </button>
+        <br/>
+        <button>
+          <img src="https://cloud.githubusercontent.com/assets/194400/11214293/4e309bf2-8d38-11e5-8d46-b347b2bd242e.png"/>
+        </button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
